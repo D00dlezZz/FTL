b45362988a9f003b1611b545ae727b23833b88ee
Mikhail <mihail.kuznetsov.99@mail.ru>
2021-10-06 14:21:47 +0300

    check

--- dist/index.js
+++ dist/index.js
@@ -2,7 +2,7 @@ const checkbox = document.getElementById('checkbox');
 
 checkbox.addEventListener('click', () => {
     if (checkbox.checked) {
-        document.querySelector('.check').style.display = "block";
+        document.querySelector('.check').style.display = "flex";
     }else {
         document.querySelector('.check').style.display = "none";
     }
